21f25c8343b960d99ee9bb4b10efb9f68c8e7173
dijith-481 <dijithdinesh@protonmail.com>
2025-04-08 16:53:07 +0530

    button styling about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  constructor(private location: Location) {}
+  goBack(): void {
+    this.location.back();
+  }
 }
