6c7dd7dab4fbcfac9e8ef0d8f8c50d33cdbcf9f0
dijith-481 <dijithdinesh@protonmail.com>
2025-04-10 01:24:42 +0530

    fixed content rendering below header

--- src/app/follow-mouse/follow-mouse.component.ts
+++ src/app/follow-mouse/follow-mouse.component.ts
@@ -13,15 +13,36 @@ export class FollowMouseComponent {
   maxSquareSize = 130;
   mouseMoveTimeout: any;
 
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
+  private handleMove(clientX: number, clientY: number) {
     this.squareSize = this.maxSquareSize;
-    this.x = event.clientX - this.maxSquareSize / 2;
-    this.y = event.clientY - this.maxSquareSize / 2;
+    this.x = clientX - this.maxSquareSize / 2;
+    this.y = clientY - this.maxSquareSize / 2;
 
     clearTimeout(this.mouseMoveTimeout);
     this.mouseMoveTimeout = setInterval(() => {
       this.squareSize -= 1;
+      if (this.squareSize <= 0) {
+        clearInterval(this.mouseMoveTimeout);
+        this.squareSize = 0;
+      }
     }, 20);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.handleMove(event.clientX, event.clientY);
+  }
+
+  @HostListener('document:touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    if (event.touches.length > 0) {
+      this.handleMove(event.touches[0].clientX, event.touches[0].clientY);
+    }
+  }
+  @HostListener('document:touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    if (event.touches.length > 0) {
+      this.handleMove(event.touches[0].clientX, event.touches[0].clientY);
+    }
   }
 }
