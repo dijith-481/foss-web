aee991a47c7be696b81d68773757a511f6f5d554
dijith-481 <dijithdinesh@protonmail.com>
2025-04-09 19:41:09 +0530

    ui tweaks header,gallery,blog

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,17 @@ import { LandingComponent } from '../landing/landing.component';
 import { HeaderComponent } from '../header/header.component';
 import { EventComponent } from '../events/event.component';
 import { BlogComponent } from '../blog/blog.component';
+import { GalleryComponent } from '../gallery/gallery.component';
 
 @Component({
   selector: 'app-home',
-  imports: [LandingComponent, HeaderComponent, EventComponent, BlogComponent],
+  imports: [
+    LandingComponent,
+    HeaderComponent,
+    EventComponent,
+    BlogComponent,
+    GalleryComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
